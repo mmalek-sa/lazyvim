be35c337d01b84e6571fe32b7831d9ff60751e3b
Mani Malek <mmalek@secureauth.com>
2025-03-11 15:01:45 -0700

    Add Golang and Jest adapters to Neotest

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -3,8 +3,10 @@ return {
 	lazy = true,
 	dependencies = {
 		"zidhuss/neotest-minitest",
+		"fredrikaverpil/neotest-golang",
+		"nvim-neotest/neotest-jest",
 	},
 	opts = {
-		adapters = { "neotest-minitest" },
+		adapters = { "neotest-minitest", "neotest-golang", "neotest-jest" },
 	},
 }
--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,7 +1,7 @@
 local map = vim.keymap.set
 
 -- stylua: ignore
-map("n", "<C-o>", function() require("dap").step_over() end, { desc = "Debug step over", remap = true })
+-- map("n", "<C-o>", function() require("dap").step_over() end, { desc = "Debug step over", remap = true })
 -- stylua: ignore
 map("n", "<C-0>", function() require("dap").step_over() end, { desc = "Debug step out", remap = true })
 -- stylua: ignore
