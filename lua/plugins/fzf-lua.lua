e3006abfa5a4081d55cce47ff1c51c8cdb521156
Mani Malek <mmalek@secureauth.com>
2025-03-11 15:44:19 -0700

    Remove unused plugins to use Lazyvim defaults

--- lua/plugins/fzf-lua.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "ibhagwan/fzf-lua",
-  keys = {
-    { "<leader>/", LazyVim.pick("live_grep"), { root = false }, desc = "Grep (cwd)" },
-    { "<leader><space>", LazyVim.pick("files"), { root = false }, desc = "Find Files (cwd)" },
-  },
-}
